5971b8a20b7f3d4d95270cb1cfc4a74c3bee3f89
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-05 13:27:12 +0100

    adds find by title method and tests

--- models/cinema.js
+++ models/cinema.js
@@ -8,5 +8,10 @@ Cinema.prototype.titlesOfFilms = function () {
   } )
 };
 
-
+Cinema.prototype.findByTitle = function (title) {
+  let found = this.films.find( function(film) {
+    return film.title === title;
+  } )
+  return found;
+};
 module.exports = Cinema;
